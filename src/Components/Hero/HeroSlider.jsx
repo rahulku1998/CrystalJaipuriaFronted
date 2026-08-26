219720d2fcc0b3fd07722a5c8a1d1bb83a4a2b32
rahulku1998 <rahul.kumawat.che15@itbhu.ac.in>
2026-08-26 13:16:52 +0530

    m

--- src/Components/Hero/HeroSlider.jsx
+++ src/Components/Hero/HeroSlider.jsx
@@ -5,102 +5,67 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import img3 from "../../assets/images/mama3.webp";
-import img4 from "../../assets/images/b.png";
-import img5 from "../../assets/images/ganesh.jpeg";
-import img6 from "../../assets/images/shiva.jpeg";
-
+import img7 from "../../assets/images/m1 (1).webp";
+import img8 from "../../assets/images/m1 (2).webp";
+import img9 from "../../assets/images/banner1.webp";
+import img10 from "../../assets/images/m1 (4).webp";
 
 export default function HeroSlider() {
   return (
     <Swiper
       modules={[Autoplay, Navigation, Pagination]}
       autoplay={{
-        delay: 3000,
+        delay: 9000,
         disableOnInteraction: false,
       }}
       loop
+      autoHeight={true}
       navigation
       pagination={{ clickable: true }}
-
       className="
-      w-full
-      h-[260px]
-      sm:h-[400px]
-      md:h-[550px]
-      lg:h-screen
+        w-full
+        [&_.swiper-button-prev]:!top-1/2
+        [&_.swiper-button-prev]:!bottom-auto
+        [&_.swiper-button-prev]:!-translate-y-1/2
+        [&_.swiper-button-prev]:!mt-0
+
+        [&_.swiper-button-next]:!top-1/2
+        [&_.swiper-button-next]:!bottom-auto
+        [&_.swiper-button-next]:!-translate-y-1/2
+        [&_.swiper-button-next]:!mt-0
       "
     >
-
-
       <SwiperSlide>
-
         <img
-          src={img4}
+          src={img7}
           alt="banner"
-          className="
-          w-full
-          h-full
-          object-cover
-          object-center
-          "
+          className="w-full h-auto block"
         />
-
       </SwiperSlide>
 
-
-
       <SwiperSlide>
-
         <img
-          src={img5}
+          src={img8}
           alt="banner"
-          className="
-          w-full
-          h-full
-          object-cover
-          object-center
-          "
+          className="w-full h-auto block"
         />
-
       </SwiperSlide>
 
-
-
       <SwiperSlide>
-
         <img
-          src={img6}
+          src={img9}
           alt="banner"
-          className="
-          w-full
-          h-full
-          object-cover
-          object-center
-          "
+          className="w-full h-auto block"
         />
-
       </SwiperSlide>
 
-
-
-
       <SwiperSlide>
-
         <img
-          src={img3}
+          src={img10}
           alt="banner"
-          className="
-          w-full
-          h-full
-          object-cover
-          object-center
-          "
+          className="w-full h-auto block"
         />
-
       </SwiperSlide>
-
-
     </Swiper>
   );
 }
